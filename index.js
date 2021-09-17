2328359aa8d6617a0723c0e50e2897163c4cba12
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2021-09-17 18:47:17 +0200

    Progress on prepareTransaction method

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const liskTransactions = require('@liskhq/lisk-transactions');
 
 class LiskChainCrypto {
   constructor({chainOptions}) {
-    this.sharedPassphrase = chainOptions.sharedPassphrase;
     this.passphrase = chainOptions.passphrase;
+    this.sharedPassphrase = chainOptions.sharedPassphrase;
   }
 
   async load() {}
@@ -25,42 +25,71 @@ class LiskChainCrypto {
     return liskCryptography.verifyData(txnHash, signatureToVerify, publicKey);
   }
 
+  _computeNonceFromTimestamp(timestamp) {
+    return BigInt(Math.round(timestamp / 1000) * 10000);
+  }
+
   async prepareTransaction(transactionData) {
     try {
-      liskTransactions.utils.validateAddress(transactionData.recipientAddress);
+      liskCryptography.validateBase32Address(transactionData.recipientAddress);
     } catch (error) {
       throw new Error(
         'Failed to prepare the transaction because the recipientAddress was invalid'
       );
     }
-    let sharedPassphrase = this.sharedPassphrase;
-    let passphrase = this.passphrase;
-    let txn = {
-      type: 0,
-      amount: transactionData.amount.toString(),
-      recipientId: transactionData.recipientAddress,
-      fee: liskTransactions.constants.TRANSFER_FEE.toString(),
-      asset: {},
-      timestamp: transactionData.timestamp,
-      senderPublicKey: liskCryptography.getAddressAndPublicKeyFromPassphrase(sharedPassphrase).publicKey
+
+    let nonce = this._computeNonceFromTimestamp(transactionData.timestamp);
+
+    let txnData = {
+      moduleID: 2,
+      assetID: 0,
+      fee: BigInt(transactionData.fee),
+      asset: {
+        amount: BigInt(transactionData.amount),
+        recipientAddress: liskCryptography.getAddressFromBase32Address(transactionData.recipientAddress),
+        data: '',
+        nonce // TODO 222 HOW to handle nonce?
+      },
+      nonce
     };
     if (transactionData.message != null) {
-      txn.asset.data = transactionData.message;
+      txnData.asset.data = transactionData.message;
     }
-    let preparedTxn = liskTransactions.utils.prepareTransaction(txn, sharedPassphrase);
+    let txn = await client.transaction.create(txnData, this.sharedPassphrase);
+    let signedTxn = await client.transaction.sign(txn, [this.sharedPassphrase, this.passphrase]);
 
-    let { signature, signSignature, signatures, ...transactionToHash } = preparedTxn;
-    let txnHash = liskCryptography.hash(liskTransactions.utils.getTransactionBytes(transactionToHash));
-    let { address: signerAddress, publicKey } = liskCryptography.getAddressAndPublicKeyFromPassphrase(passphrase);
+    let { address: sharedAddress, publicKey: sharedPublicKey } = liskCryptography.getAddressAndPublicKeyFromPassphrase(this.sharedPassphrase);
+    let { address: signerAddress, publicKey: signerPublicKey } = liskCryptography.getAddressAndPublicKeyFromPassphrase(this.passphrase);
+
+    let preparedTxn = {
+      moduleID: signedTxn.moduleID,
+      assetID: signedTxn.assetID,
+      fee: signedTxn.fee.toString(),
+      asset: {
+        amount: signedTxn.asset.amount.toString(),
+        recipientAddress: signedTxn.asset.recipientAddress.toString('hex'),
+        data: signedTxn.data,
+        nonce: signedTxn.nonce.toString()
+      },
+      nonce: signedTxn.nonce.toString(),
+      senderPublicKey: signedTxn.senderPublicKey.toString('hex'),
+      signatures: [
+        {
+          signerAddress: sharedAddress,
+          publicKey: sharedPublicKey.toString('hex'),
+          signature: signedTxn.signatures[0].toString('hex')
+        }
+      ],
+      id: signedTxn.id.toString('hex')
+    };
 
     // The signature needs to be an object with a signerAddress property, the other
     // properties are flexible and depend on the requirements of the underlying blockchain.
     let multisigTxnSignature = {
       signerAddress,
-      publicKey,
-      signature: liskCryptography.signData(txnHash, passphrase)
+      publicKey: signerPublicKey.toString('hex'),
+      signature: signedTxn.signatures[1].toString('hex')
     };
-    preparedTxn.signatures = [];
 
     return {transaction: preparedTxn, signature: multisigTxnSignature};
   }
